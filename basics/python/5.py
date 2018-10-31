3c9033e74a0e0a7f4d599dd821a59d34fcb3a0f7
VitaliiS-983 <42441367+VitaliiS-983@users.noreply.github.com>
2018-10-31 21:45:18 +0200

    Add files via upload

---
+++ basics/python/5.py
@@ -0,0 +1,7 @@
+def histogram( anarray ):
+        "A Histogram Function"
+        for x in range(0,len(anarray)):
+                print 'X' * anarray[x];
+        return;
+
+histogram([4,2,5,9,4]);
